b10cb9a70abadfdd87e6896052053e35f2147330
jeanney11 <jclayrz11@gmail.com>
2022-06-27 06:41:06 -0300

    Cambio Final

--- src/Componentes/AddNow/AddNow.js
+++ src/Componentes/AddNow/AddNow.js
@@ -39,40 +39,40 @@ function AddNow(props) {
 
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco"
+      image: "https://elpopular.cronosmedia.glr.pe/original/2022/03/12/621cf4a77841311020324106.jpg",
+      caption: "Morbius"
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland"
+      image: "https://www.guatemala.com/fotos/2022/02/Estreno-de-la-pelicula-The-Batman-en-Guatemala-Marzo-2022-885x500.png",
+      caption: "Batman"
     },
     {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling"
+      image: "https://moviesroom.pl/wp-content/uploads/2022/05/Top-Gun-Maverick.jpg",
+      caption: "Top Gun"
     },
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco"
+      image: "https://www.cinepremiere.com.mx/wp-content/uploads/2021/12/Avatar-2-James-Cameron-peliculas-2022-900x506.jpg",
+      caption: "Avatar"
     },
     {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland"
+      image: "https://phantom-marca.unidadeditorial.es/8085812b4fc214a7c7deca3e52d605a2/resize/1200/f/webp/assets/multimedia/imagenes/2022/02/11/16445345407827.jpg",
+      caption: "Jurasic World"
     },
     {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling"
+      image: "https://m.cinesargentinos.com.ar/static/archivos/56370",
+      caption: "Los Secretos de Dumbledore"
     },
     {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco"
+      image: "https://media.vandal.net/i/1200x630/12-2021/2021121515523463_1.jpg",
+      caption: "Uncharted"
     },
     {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland"
+      image: "https://phantom-marca.unidadeditorial.es/bb80c3bd0d9d9027aa526ec95e271326/resize/1320/f/jpg/assets/multimedia/imagenes/2022/06/13/16551302986436.jpg",
+      caption: "LightYear"
     },
     {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling"
+      image: "https://eus-www.sway-cdn.com/s/ySQJgFqWsreA5mGw/images/gWMAwJ8DtrT438?quality=1920&allowAnimation=true",
+      caption: "La Ciudad Perdida"
     }
   ];
 
